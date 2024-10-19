bf9308a03c4de3262af98ba207babfd0adbc39a5
Fatima Naz <naz781@gmail.com>
2024-10-19 11:48:40 -0400

    create dog list app

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 import { Routes, Route, NavLink } from 'react-router-dom'
 import DogForm from './DogForm'
 import DogsList from './DogsList'
 
+
 export default function App() {
+  const [dogs, setDogs] = useState([])
+  const [curentDogId, setCurrentDog] = useState(null)
+
+  useEffect(() => {getDogs()},[])
+
+const getDogs =  () => {
+    fetch('/api/dogs')
+    .then(res => {
+      if(!res.ok)throw new Error("Problem GETing dogs")
+        return res.json()})
+    .then(dogs => setDogs(dogs))
+    .catch(err => console.error(err))
+  }
   return (
     <div>
       <nav>
@@ -11,8 +25,12 @@ export default function App() {
         <NavLink to="/form">Form</NavLink>
       </nav>
       <Routes>
-        <Route path="/" element={<DogsList />} />
-        <Route path="/form" element={<DogForm />} />
+        <Route path="/" element={<DogsList 
+          dogs={dogs} getDogs={getDogs} setCurrentDog={setCurrentDog}/>} />
+        <Route path="/form" element={<DogForm 
+          dog={curentDogId && dogs.find(dog => dog.id === curentDogId)}
+          getDogs={getDogs} reset = {() => setCurrentDog(null)}
+        />} />
       </Routes>
     </div>
   )
--- frontend/components/DogForm.js
+++ frontend/components/DogForm.js
@@ -1,12 +1,63 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const initialForm = { name: '', breed: '', adopted: false }
 
 // Use this form for both POST and PUT requests!
-export default function DogForm() {
+export default function DogForm({ dog, getDogs, reset }) {
+  const navigate = useNavigate()
   const [values, setValues] = useState(initialForm)
+  const [breeds, setBreeds] = useState([])
+  useEffect(() => {
+    fetch('/api/dogs/breeds')
+      .then(res => res.json())
+      .then(breeds => setBreeds(breeds.toSorted()))
+      .catch(err => console.error(err))
+  },[])
+  useEffect(() => {
+    if (dog) {
+      setValues(dog)
+    } else {
+      setValues(initialForm)
+    }
+  },[dog])
+  const postDog=()=>{
+    fetch('/api/dogs', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(values)
+    })
+    .then(res => {
+      if(!res.ok)throw new Error("Problem POSTing dog")
+      getDogs()
+      navigate("/")
+    })
+    .catch(err => console.error(err))
+  }
+  const putDog =()=>{
+    fetch(`/api/dogs/${values.id}`, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(values)
+    })
+    .then(res => {
+      if(!res.ok)throw new Error("Problem PUTing dog")
+      getDogs()
+      reset()
+      navigate("/")
+    })
+    .catch(err => console.error(err))
+
+  }
+  const onReset=(e)=>{
+    e.preventDefault()
+    setValues(initialForm)
+    reset()
+
+  }
   const onSubmit = (event) => {
     event.preventDefault()
+    dog ? putDog() : postDog()
   }
   const onChange = (event) => {
     const { name, value, type, checked } = event.target
@@ -17,39 +68,28 @@ export default function DogForm() {
   return (
     <div>
       <h2>
-        Create Dog
+      {dog?"Update Dog":"Create Dog"}
       </h2>
       <form onSubmit={onSubmit}>
         <input
-          name="name"
-          value={values.name}
-          onChange={onChange}
-          placeholder="Name"
-          aria-label="Dog's name"
+          name="name" value={values.name} onChange={onChange} placeholder="Name" aria-label="Dog's name"
         />
         <select
-          name="breed"
-          value={values.breed}
-          onChange={onChange}
-          aria-label="Dog's breed"
+          name="breed" value={values.breed} onChange={onChange} aria-label="Dog's breed"
         >
           <option value="">---Select Breed---</option>
-          {/* Populate this dropdown using data obtained from the API */}
+          {breeds.map(br => <option key={br}>{br}</option>)}
         </select>
         <label>
           Adopted: <input
-            type="checkbox"
-            name="adopted"
-            checked={values.adopted}
-            onChange={onChange}
-            aria-label="Is the dog adopted?"
+            type="checkbox" name="adopted" checked={values.adopted} onChange={onChange} aria-label="Is the dog adopted?"
           />
         </label>
         <div>
           <button type="submit">
-            Create Dog
+            {dog?"Update Dog":"Create Dog"}
           </button>
-          <button aria-label="Reset form">Reset</button>
+          <button onClick={onReset} aria-label="Reset form">Reset</button>
         </div>
       </form>
     </div>
